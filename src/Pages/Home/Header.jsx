491d42b08c313b43cb82abfb65e224454781db18
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2021-03-14 01:51:50 +0500

    background video complete

--- src/Pages/Home/Header.jsx
+++ src/Pages/Home/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Londan from "../../assets/videos/London City - The United Kingdom_2.mp4";
 
 function Header(props) {
     return (
         <div className='headerHome pt-4'>
+            <video autoPlay muted loop className='headerHome-video'>
+                <source src={Londan} type='video/mp4' />
+            </video>
             <div className="z-10 position-relative">
                 <h1 className="text-right font-weight-thin f-93">
                     we provide
