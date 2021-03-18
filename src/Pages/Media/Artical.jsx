b96085292a0a71f58aaea27e9e0a79dbf3529243
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2021-03-18 22:00:40 +0500

    uplade

--- src/Pages/Media/Artical.jsx
+++ src/Pages/Media/Artical.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MyNavBar from '../../components/MyNavBar';
 import { media } from "../Media/media";
 import { CgScrollV } from "react-icons/cg";
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import NavOpenerBtn from '../../components/NavOpenerBtn';
 import Logo from '../../assets/img/logo.png'
 import { FaFilePdf } from 'react-icons/fa'
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
 function Artical(props) {
     const { id } = props.match.params
     const [menuOpener, setMenuOpener] = useState(false)
@@ -20,8 +21,9 @@ function Artical(props) {
             return null
     })
     // const onScroll = (e) => {
+    //     console.log(window.pageYOffset );
     //     var height = window.innerHeight
-    //     // console.log(height);
+    //     console.log(height);
     //     var st = window.pageYOffset  // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
     //     if (st > lastScrollTop) {
     //         if (window.pageYOffset < height) {
@@ -30,7 +32,7 @@ function Artical(props) {
     //             setticking(true)
     //         }
     //     } else {
-    //         if (window.pageYOffset) {
+    //         if (window.pageYOffset<height) {
     //             window.scrollTo(0, 0);
     //             console.log('up');
     //             setticking(false)
@@ -40,29 +42,45 @@ function Artical(props) {
 
     // }
     // useEffect(() => {
-    //     window.addEventListener('scroll', onScroll);
+    //     window.addEventListener('wheel', onScroll);
 
     //     return () => {
-    //         window.removeEventListener('scroll', onScroll);
+    //         window.removeEventListener('wheel', onScroll);
     //     };
     // });
-    return (
-        <div>
+    const goDown = () => {
+        if (window.pageYOffset < window.innerHeight) {
+            window.scrollTo(0, 1000);
+            console.log('up');
+        }
+    }
+    const goTop = () => {
+        console.log('up');
+        // if (window.pageYOffset <= window.innerHeight)
+        //     window.scrollTo(0, 0);
+    }
 
-            {
-                data === undefined ?
-                    <h1 className='text-center pt-5'>
-                        No Data against ID
+    return (
+        <ReactScrollWheelHandler
+            upHandler={(e) => goTop()}
+            downHandler={(e) => goDown()}
+        >
+            <div>
+
+                {
+                    data === undefined ?
+                        <h1 className='text-center pt-5'>
+                            No Data against ID
             </h1> :
-                    <div>
-                        <div className="articalBack" style={{ background: `url(${data.img})` }}>
-                            <div className={` position-absolute w-100 left-0 top-0 py-2 shadow }`}>
-                                <div className="px-3 position-relative">
-                                    <div className="myNavBar">
-                                        <Link to='/' className='d-none d-md-block'>
-                                            <img src={Logo} className='myNavBar-logo' alt="logo" />
-                                        </Link>
-                                        {/* {
+                        <div>
+                            <div className="articalBack" style={{ background: `url(${data.img})` }}>
+                                <div className={` position-absolute w-100 left-0 top-0 py-2 shadow }`}>
+                                    <div className="px-3 position-relative">
+                                        <div className="myNavBar">
+                                            <Link to='/' className='d-none d-md-block'>
+                                                <img src={Logo} className='myNavBar-logo' alt="logo" />
+                                            </Link>
+                                            {/* {
                             props.title !== undefined ?
                                 <div className="f-20 d-block d-md-none navBorder pr-3">
                                     {
@@ -71,42 +89,42 @@ function Artical(props) {
                                 </div> :
                                 <></>
                         } */}
-                                        <NavOpenerBtn active={menuOpener} setActive={setMenuOpener} />
-                                        <ul className={`myNavBar-menu ${menuOpener ? 'active' : ''}`}>
-                                            <li>
-                                                <Link to='/'>
-                                                    About INTRIVA
+                                            <NavOpenerBtn active={menuOpener} setActive={setMenuOpener} />
+                                            <ul className={`myNavBar-menu ${menuOpener ? 'active' : ''}`}>
+                                                <li>
+                                                    <Link to='/'>
+                                                        About INTRIVA
                                 </Link>
-                                            </li>
-                                            <li>
-                                                <Link to='/whoweare'>
-                                                    Who we are
+                                                </li>
+                                                <li>
+                                                    <Link to='/whoweare'>
+                                                        Who we are
                                 </Link>
-                                            </li>
-                                            <li>
-                                                <Link to='/whatwedo'>
-                                                    What We Do
+                                                </li>
+                                                <li>
+                                                    <Link to='/whatwedo'>
+                                                        What We Do
                                 </Link>
-                                            </li>
-                                            <li>
-                                                <Link to='/media'>
-                                                    In the Media
+                                                </li>
+                                                <li>
+                                                    <Link to='/media'>
+                                                        In the Media
                                 </Link>
-                                            </li>
-                                            <li>
-                                                <Link to='/'>
-                                                    Make Contact
+                                                </li>
+                                                <li>
+                                                    <Link to='/'>
+                                                        Make Contact
                                 </Link>
-                                            </li>
-                                            <li>
-                                                <Link to='/'>
-                                                    For INTRIVA
+                                                </li>
+                                                <li>
+                                                    <Link to='/'>
+                                                        For INTRIVA
                                 </Link>
-                                            </li>
-                                        </ul>
+                                                </li>
+                                            </ul>
 
-                                    </div>
-                                    {/* {
+                                        </div>
+                                        {/* {
                         data.title !== undefined ?
                             <div className={`text-uppercase f-20 pl-3 d-none d-md-block text-md-right pb-3 pt-2 ${scrollCheck ? 'position-md-absolute top-50 ' : ' navBorder'} ${props.artical?'navArtical':'navTitle'}`}>
                                 {
@@ -116,48 +134,49 @@ function Artical(props) {
                             :
                             <></>
                     } */}
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className='content'>
-                                <h1 className="text-center">
-                                    {data.title}
-                                </h1>
-                                <div className='text-center f-25'>
-                                    <span className="upperLine">
+                                <div className='content'>
+                                    <h1 className="text-center">
+                                        {data.title}
+                                    </h1>
+                                    <div className='text-center f-25'>
+                                        <span className="upperLine">
+                                            {
+                                                data.date
+                                            }
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="scrollDownBtn">
+                                    <CgScrollV />
+                                </div>
+                            </div>
+                            <div className="articalContent">
+                                <MyNavBar tab={1} title={data.title} artical={true} />
+                                <div className="container pt-5">
+                                    <div className="d-flex justify-content-between">
+                                        <div>
+                                            {data.date}
+                                        </div>
+                                        <div className='f-30'>
+                                            <FaFilePdf />
+                                        </div>
+                                    </div>
+                                    <div className='mt-3'>
                                         {
-                                            data.date
+                                            data.detail
                                         }
-                                    </span>
-                                </div>
-                            </div>
-                            <div className="scrollDownBtn">
-                                <CgScrollV />
-                            </div>
-                        </div>
-                        <div className="articalContent">
-                            <MyNavBar tab={1} title={data.title} artical={true} />
-                            <div className="container pt-5">
-                                <div className="d-flex justify-content-between">
-                                    <div>
-                                        {data.date}
-                                    </div>
-                                    <div className='f-30'>
-                                        <FaFilePdf />
                                     </div>
                                 </div>
-                                <div className='mt-3'>
-                                    {
-                                        data.detail
-                                    }
+                                <div className="bottom-0 position-absolute w-100">
+                                    <Footer />
                                 </div>
                             </div>
-                            <div className="bottom-0 position-absolute w-100">
-                                <Footer />
-                            </div>
-                        </div>
-                    </div>}
-        </div>
+                        </div>}
+            </div>
+        </ReactScrollWheelHandler>
     );
 }
 
