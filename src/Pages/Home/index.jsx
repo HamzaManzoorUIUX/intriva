14c942e1bf824b610cb0444d3619d938cb6bb7ce
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2021-03-20 01:19:02 +0500

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import MyNavBar from '../../components/MyNavBar';
 import sectionOne from '../../assets/img/Mask Group 6.png'
@@ -10,7 +10,7 @@ import Fade from 'react-reveal/Fade';
 import ScrollToTopOnMount from '../../components/ScrollToTop';
 function Home(props) {
     const { section } = props.match.params
-    const [lastScrollTop, setlastScrollTop] = useState(section!==undefined?false:true)
+    const [lastScrollTop, setlastScrollTop] = useState(section !== undefined ? false : true)
 
     const goDown = () => {
         if (window.pageYOffset >= 0) {
@@ -24,104 +24,108 @@ function Home(props) {
     }
     return (
         <ReactScrollWheelHandler
-        upHandler={(e) => goTop()}
-        downHandler={(e) => goDown()}
-    >
-        <div className='position-relative'>
-            
-            {/* <PreLoader /> */}
-            <ScrollToTopOnMount/>
-            <Fade top  when={lastScrollTop} big>
-            <div className={`position-absolute w-100 ${lastScrollTop ? 'z-6000' : 'z-0'}`}>
-            <Header goDown={goDown}/>
-            </div>
-            </Fade>
-            <MyNavBar tab={1} title={'ABOUT INTRIVA'} />
-            <div className='container-fluid homeBack'>
-                <div className="row align-items-center py-5">
-                    <Fade right big>
-                        <div className="col-md-6 d-none d-md-block pl-0">
-                            <img src={sectionOne} alt="sectionOne" className='sectionImg' />
-                        </div>
-                    </Fade>
-                    <div className="col-md-6 col-lg-5 col-xl-4">
-                        <Fade left big>
-                            <div>
-                                <h4 className='text-primary '>
-                                    <span className="upperLine">
-                                        Founded in 2017
-                      </span>
-                                </h4>
-                                <div>
-                                    Founded in 2017 , Intriva is a privately held alternative alternative asset managent firmspecialising in asset-backed debt and equity special situations across Western Europe
-                  </div>
-                            </div>
-                        </Fade>
-                        <Fade left big>
-                            <div>
-                                <h4 className='text-primary '>
-                                    <span className="upperLine">
-                                        Intriva provides
-                      </span>
-                                </h4>
-                                <div>
-                                    Intriva provides an attractive risk adjusted return profile to our investors, and bespoke capitalsolutions to our partners throughout Western Europe.
-                  </div>
-                            </div>
-                        </Fade>
-                        <Fade left big>
-                            <div>
-                                <h4 className='text-primary '>
-                                    <span className="upperLine">
-                                        Intriva invests
-                      </span>
-                                </h4>
-                                <div>
-                                    Intriva invests throughout the capital structure in both single name positions, and also supportsthematic growth opportunities with large scale roll out strategies.
-                  </div>
-                            </div>
-                        </Fade>
-                        <Fade left big>
-                            <div>
-                                <h4 className='text-primary '>
-                                    <span className="upperLine">
-                                        Intriva employs
-                      </span>
-                                </h4>
-                                <div>
-                                    As of December 31, 2020, Intriva employs a staff of 16 people, including 11 investmentprofessionals, in its London headquarters and affiliated offices in Frankfurt and Madrid.
-                  </div>
-                            </div>
-                        </Fade>
-                    </div>
-                </div>
-                <div className="row align-items-center py-5">
-                    <div className="col-md-6 pl-md-5">
-                        <Fade right big>
-                            <h4>
-                                Culture
-                   </h4>
-                            <div>
-                                <p>
-                                    We take pride in our collaborative culture, which harnesses camaraderie and the balance of skills and experience which our team members collectively bring to bear.
-                       </p>
-                                <p>
+            upHandler={(e) => goTop()}
+            downHandler={(e) => goDown()}
+        >
+            <div className='position-relative'>
 
-                                    As a young firm, we are entrepreneurial and hungry to delivery exceptional returns for our investors.  We are creative about our investments and how they are structured.  We work hard and pursue opportunities with tenacity and thoroughness.
-                       </p>
-                            </div>
+                {/* <PreLoader /> */}
+                <ScrollToTopOnMount />
+                <Fade top when={lastScrollTop} big>
+                    <div className={`position-absolute w-100 ${lastScrollTop ? 'z-6000' : 'z-0'}`}>
+                        <Header goDown={goDown} />
+                    </div>
+                </Fade>
+                <MyNavBar tab={1} title={'ABOUT INTRIVA'} />
+                <div className="homeBack">
+                    <div className='container '>
+                        <div className="row align-items-center py-5">
+                            <Fade right big>
+                                <div className="col-md-6 d-none d-md-block pl-0">
+                                    <img src={sectionOne} alt="sectionOne" className='sectionImg' />
+                                </div>
                             </Fade>
-               </div>
-                        <div className="col-md-6 text-right pr-0 d-none d-md-block">
-                        <Fade left big>
-                            <img src={sectionTwo} alt="sectionTwos" className='sectionImg' />
-                        </Fade>
+                            <div className="col-md-6">
+                                <Fade left big>
+                                    <div>
+                                        <h4 className='text-primary '>
+                                            <span className="upperLine">
+                                                Founded in 2017
+                      </span>
+                                        </h4>
+                                        <div>
+                                            Founded in 2017 , Intriva is a privately held alternative alternative asset managent firmspecialising in asset-backed debt and equity special situations across Western Europe
+                  </div>
+                                    </div>
+                                </Fade>
+                                <Fade left big>
+                                    <div>
+                                        <h4 className='text-primary '>
+                                            <span className="upperLine">
+                                                Intriva provides
+                      </span>
+                                        </h4>
+                                        <div>
+                                            Intriva provides an attractive risk adjusted return profile to our investors, and bespoke capitalsolutions to our partners throughout Western Europe.
+                  </div>
+                                    </div>
+                                </Fade>
+                                <Fade left big>
+                                    <div>
+                                        <h4 className='text-primary '>
+                                            <span className="upperLine">
+                                                Intriva invests
+                      </span>
+                                        </h4>
+                                        <div>
+                                            Intriva invests throughout the capital structure in both single name positions, and also supportsthematic growth opportunities with large scale roll out strategies.
+                  </div>
+                                    </div>
+                                </Fade>
+                                <Fade left big>
+                                    <div>
+                                        <h4 className='text-primary '>
+                                            <span className="upperLine">
+                                                Intriva employs
+                      </span>
+                                        </h4>
+                                        <div>
+                                            As of December 31, 2020, Intriva employs a staff of 16 people, including 11 investmentprofessionals, in its London headquarters and affiliated offices in Frankfurt and Madrid.
+                  </div>
+                                    </div>
+                                </Fade>
+                            </div>
+                        </div>
+                        <div className="row align-items-center py-5">
+                            <div className="col-md-6  pl-md-5">
+                                <Fade right big>
+                                    <h4>
+                                        Culture
+                   </h4>
+                                    <div>
+                                        <p>
+                                            We take pride in our collaborative culture, which harnesses camaraderie and the balance of skills and experience which our team members collectively bring to bear.
+                       </p>
+                                        <p>
+
+                                            As a young firm, we are entrepreneurial and hungry to delivery exceptional returns for our investors.  We are creative about our investments and how they are structured.  We work hard and pursue opportunities with tenacity and thoroughness.
+                       </p>
+                                    </div>
+                                </Fade>
+                            </div>
+                            <div className="col-md-6 text-right pr-0 d-none d-md-block">
+                                <Fade left big>
+                                    <img src={sectionTwo} alt="sectionTwos" className='sectionImg' />
+                                </Fade>
+                            </div>
                         </div>
                     </div>
+
+
                 </div>
                 <Footer />
             </div>
-            </ReactScrollWheelHandler>
+        </ReactScrollWheelHandler>
     );
 }
 
