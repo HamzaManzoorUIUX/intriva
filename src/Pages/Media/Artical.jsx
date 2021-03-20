077a05aa6f795783f0f3793c61a58211c86b03f3
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2021-03-20 20:03:53 +0500

    artical page ok

--- src/Pages/Media/Artical.jsx
+++ src/Pages/Media/Artical.jsx
@@ -7,16 +7,21 @@ import { Link } from 'react-router-dom';
 import NavOpenerBtn from '../../components/NavOpenerBtn';
 import Logo from '../../assets/img/logo.png'
 import { FaFilePdf } from 'react-icons/fa'
-import ReactScrollWheelHandler from "react-scroll-wheel-handler";
-import Fade from 'react-reveal/Fade';
 import Pdf from "react-to-pdf";
-import parse from "html-react-parser";
+import ReactPageScroller from "react-page-scroller";
+import parse from "html-react-parser"; 
 const ref = React.createRef();
 function Artical(props) {
     const { id } = props.match.params
     const [menuOpener, setMenuOpener] = useState(false)
-    const [lastScrollTop, setlastScrollTop] = useState(true)
-    // const [ticking, setticking] = useState(false)
+    const [currentPage, setcurrentPage] = useState(0)
+    const handlePageChange = number => {
+        setcurrentPage(number)
+    };
+
+    const handleBeforePageChange = number => {
+        console.log(number);
+    };
     const data = media.find(obj => {
         if (obj.id === id) {
             return obj
@@ -24,24 +29,11 @@ function Artical(props) {
         else
             return null
     })
-  
-    const goDown = () => {
-        if (window.pageYOffset >= 0) {
-            setlastScrollTop(false)
-        }
-    }
-    const goTop = () => {
-        if (window.pageYOffset <= 0) {
-            setlastScrollTop(true)
-        }
-    }
+
+
 
     return (
-        <ReactScrollWheelHandler
-            upHandler={(e) => goTop()}
-            downHandler={(e) => goDown()}
-        >
-            <div id=''>
+        <div id=''>
 
                 {
                     data === undefined ?
@@ -49,70 +41,73 @@ function Artical(props) {
                             No Data against ID
             </h1> :
                         <div ref={ref}>
-                            <Fade top when={lastScrollTop} big>
-                                <div className={`articalBack position-absolute ${lastScrollTop ? 'z-6000' : 'z-0'}`} style={{ background: data.img!==undefined?`url('${data.img}')`:'white' }}>
-                                    <div className={`z-10 position-absolute w-100 left-0 top-0 py-2 }`}>
-                                        <div className="px-3 position-relative">
-                                            <div className="myNavBar">
-                                                <Link to='/' className='d-none d-md-block'>
-                                                    <img src={Logo} className='myNavBar-logo' alt="logo" />
-                                                </Link>
-                                                <NavOpenerBtn active={menuOpener} setActive={setMenuOpener} />
-                                                <ul className={`myNavBar-menu ${menuOpener ? 'active' : ''}`}>
-                                                    <li>
-                                                        <Link to='/'>
-                                                            About INTRIVA
+                            <ReactPageScroller
+                                pageOnChange={handlePageChange}
+                                onBeforePageScroll={handleBeforePageChange}
+                                customPageNumber={currentPage}
+                            >
+                            <div className={`articalBack`} style={{ background: data.img !== undefined ? `url('${data.img}')` : 'white' }}>
+                                <div className={`z-10 position-absolute w-100 left-0 top-0 py-2 }`}>
+                                    <div className="px-3 position-relative">
+                                        <div className="myNavBar">
+                                            <Link to='/' className='d-none d-md-block'>
+                                                <img src={Logo} className='myNavBar-logo' alt="logo" />
+                                            </Link>
+                                            <NavOpenerBtn active={menuOpener} setActive={setMenuOpener} />
+                                            <ul className={`myNavBar-menu ${menuOpener ? 'active' : ''}`}>
+                                                <li>
+                                                    <Link to='/'>
+                                                        About INTRIVA
                                 </Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link to='/whoweare'>
-                                                            Who we are
+                                                </li>
+                                                <li>
+                                                    <Link to='/whoweare'>
+                                                        Who we are
                                 </Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link to='/whatwedo'>
-                                                            What We Do
+                                                </li>
+                                                <li>
+                                                    <Link to='/whatwedo'>
+                                                        What We Do
                                 </Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link to='/media'>
-                                                            In the Media
+                                                </li>
+                                                <li>
+                                                    <Link to='/media'>
+                                                        In the Media
                                 </Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link to='/'>
-                                                            Make Contact
+                                                </li>
+                                                <li>
+                                                    <Link to='/'>
+                                                        Make Contact
                                 </Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link to='/'>
-                                                            For INTRIVA
+                                                </li>
+                                                <li>
+                                                    <Link to='/'>
+                                                        For INTRIVA
                                 </Link>
-                                                    </li>
-                                                </ul>
+                                                </li>
+                                            </ul>
 
-                                            </div>
-                           
                                         </div>
-                                    </div>
 
-                                    <div className='content'>
-                                        <h1 className="text-center">
-                                            {data.title}
-                                        </h1>
-                                        <div className='text-center f-25'>
-                                            <span className="upperLine">
-                                                {
-                                                    data.date
-                                                }
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div className="scrollDownBtn" onClick={goDown}>
-                                        <CgScrollV />
                                     </div>
                                 </div>
-                            </Fade>
+
+                                <div className='content'>
+                                    <h1 className="text-center">
+                                        {data.title}
+                                    </h1>
+                                    <div className='text-center f-25'>
+                                        <span className="upperLine">
+                                            {
+                                                data.date
+                                            }
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="scrollDownBtn" onClick={() => handlePageChange(1)}>
+                                    <CgScrollV />
+                                </div>
+                            </div>
                             <div className="articalContent">
                                 <MyNavBar tab={1} artical={true} />
                                 <div className="container pt-5">
@@ -127,19 +122,19 @@ function Artical(props) {
                                         </Pdf>
 
                                     </div>
-                                 <div className='pt-3 pb-5'>
-                                 {
-                                 parse(data.detail)
-                                 }
-                                 </div>
+                                    <div className='pt-3 pb-5'>
+                                        {
+                                            parse(data.detail)
+                                        }
+                                    </div>
                                 </div>
                                 <div className=" w-100">
                                     <Footer />
                                 </div>
                             </div>
+                        </ReactPageScroller>
                         </div>}
             </div>
-        </ReactScrollWheelHandler>
     );
 }
 
