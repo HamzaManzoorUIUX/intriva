144874c8c54eeeb82d7bcea0e87a2a40f7eff141
Hamza Manzoor <35131572+HamzaManzoorUIUX@users.noreply.github.com>
2021-04-27 23:56:38 +0500

    done 2nd revision

--- src/components/StickyFooter.jsx
+++ src/components/StickyFooter.jsx
@@ -5,24 +5,24 @@ import PRI from "../assets/img/UN+PRI+logo+copy.png";
 
 function StickyFooter(props) {
     const [show, setShow] = useState(false);
+    const [show2, setShow2] = useState(false);
 
     return (
 <>
 <div className={`ScreenSide ${props.home!==undefined?'active':''} ${props.currentPage===2?'mt-fotPic':''}`}>
         <div className="d-flex px-2 h-100 align-items-center " style={{backgroundColor:"#161E26",borderTopRightRadius:'20px'}}>
         <img src={PRI} onClick={()=>setShow(true)} className="curser" height="30px" alt='img' />
-        <img src={LinkedIn} onClick={()=>setShow(true)} className="curser"  height="30px" alt='img' />
+        <img src={LinkedIn} onClick={()=>setShow2(true)} className="curser"  height="30px" alt='img' />
         </div>
 </div>
         <Modal show={show} onHide={()=>setShow(false)} centered>
         <Modal.Header closeButton>
-          <Modal.Title>PRI and LinkedIn Links</Modal.Title>
+          <Modal.Title>
+          Environmental, Social and Corporate Governance
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
          <div className='card p-2 mb-3'>
-         <h3>
-          Environmental, Social and Corporate Governance
-          </h3>
           <p>
           We are committed to driving sustainable value and have integrated Environmental, Social and Corporate Governance into our approach, with an in-depth ESG policy. We are signatories of Principals for Responsible Investment (PRI).
           </p>
@@ -30,10 +30,14 @@ function StickyFooter(props) {
           Click here to visit the PRI Website
           </a>
          </div>
-         <div className='card p-2 my-3'>
-           <h3>
-           LinkedIn
-           </h3>
+       </Modal.Body>
+      </Modal>
+        <Modal show={show2} onHide={()=>setShow2(false)} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>LinkedIn</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <div className='card p-2 my-3'>
            <p>
            Please visit us and follow us on social media.
            </p>
